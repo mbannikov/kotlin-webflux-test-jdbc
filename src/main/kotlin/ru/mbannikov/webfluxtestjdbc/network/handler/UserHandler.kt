a1a02700c905a2941da73d5cd9e31280cf94d8dc
Maksim Bannikov <spystieber@gmail.com>
2019-05-05 22:18:47 +0300

    Hot fix: add exception handling to controller

--- src/main/kotlin/ru/mbannikov/webfluxtestjdbc/network/handler/UserHandler.kt
+++ src/main/kotlin/ru/mbannikov/webfluxtestjdbc/network/handler/UserHandler.kt
@@ -7,6 +7,7 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toMono
 import ru.mbannikov.webfluxtestjdbc.domain.UserAuthenticationService
+import ru.mbannikov.webfluxtestjdbc.domain.UserExistsException
 import ru.mbannikov.webfluxtestjdbc.domain.UserRegisterService
 import ru.mbannikov.webfluxtestjdbc.domain.Username
 import ru.mbannikov.webfluxtestjdbc.network.dto.*
@@ -40,6 +41,12 @@ class UserHandler(
         return request.bodyToMono(RegisterRequest::class.java)
             .flatMap { registerService.register(Username(it.username), it.email, it.password).toMono() }
             .flatMap { ServerResponse.ok().syncBody(successResponse) }
-            .onErrorResume { ServerResponse.ok().syncBody(RegisterResponse(FAIL_STATUS, "Error during registration")) }
+            .onErrorResume {
+                val responseMessage = when (it) {
+                    is UserExistsException -> it.message
+                    else -> "Server error"
+                }
+                ServerResponse.status(HttpStatus.UNAUTHORIZED).syncBody(RegisterResponse(FAIL_STATUS, responseMessage))
+            }
     }
 }
